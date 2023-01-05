daa08934147cc5ce550e25c6c2aad0a2356e9005
David Goertsen <davidgoertsen@gmail.com>
2023-01-04 16:58:50 -0800

    Added single image plotting to plot

--- cellseg/plot.py
+++ cellseg/plot.py
@@ -1,6 +1,12 @@
+# Image processing tools
+import skimage.io
+import glob
+
 # Plotting tools
 import bokeh
 from bokeh.plotting import figure
+
+import cellseg.quant
 
 def imshow(im, cmap=None):
     p = bokeh.plotting.figure(frame_height=400,
@@ -15,7 +21,7 @@ def imshow(im, cmap=None):
     return(p)
 
 def show_two_ims(im_1, 
-                 im_2, 
+                 im_2,
                  color_mapper=None):
     
     """Convenient function for showing two images side by side."""
@@ -47,3 +53,28 @@ def show_two_ims(im_1,
     
     return bokeh.layouts.gridplot([p_1, p_2], ncols=2)
         
+def single_image_viewer(path, round_, plate, well): 
+        # Set the file directory to those images
+    if well < 10:
+        well_directory = path + '/round_' + str(round_) + '/plate_' + str(plate) + '/XY0'+ str(well) +'/'
+    else:
+        well_directory = path + '/round_' + str(round_) + '/plate_' + str(plate) + '/XY'+ str(well) +'/'
+        
+    # Collect the images at that well
+    file_list = glob.glob(well_directory + '*.tif')
+    
+    # Initialize both the signal and autofluorescence image:
+    im_sig = skimage.img_as_float(skimage.io.imread(file_list[0])[:,:,1])
+    im_bf = skimage.img_as_float(skimage.io.imread(file_list[1])[:,:])
+    im_dapi = skimage.img_as_float(skimage.io.imread(file_list[2])[:,:,2])
+
+    # Perform the brightfield segmentation
+    brightfield_areas, total_area = cellseg.quant.brightfield_segmentation(im_bf)
+
+    # Perform the signal segmentation
+    signal_areas, signal_total_area = cellseg.quant.signal_segmentation(im_sig)
+    
+    bf_plot = cellseg.plot.show_two_ims(im_bf, brightfield_areas, color_mapper=[bokeh.palettes.gray(256), bokeh.palettes.gray(256)])
+    sig_plot = cellseg.plot.show_two_ims(im_sig, signal_areas, color_mapper=[bokeh.palettes.gray(256), bokeh.palettes.gray(256)])
+    
+    return(bf_plot, sig_plot)
